5bd01bbec9e6ab374a2bfd7378a0991a45cfb0c6
infarion@gmail.com <infarion@gmail.com>
2019-12-23 15:16:55 +0200

    Добавьте файлы проекта.

---
+++ exam/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /*Создать класс "Произведение искусства". 
+     * Поля: Художник (скульптор, живописец, etc),
+     * Название произведения искусства, 
+     * Год создания. 
+Создать массив объектов-произведений искусства (данные можно прочитать из файла).
+Отсортировать массив по художникам, по дате создания. 
+Вывести отсортированный массив на консоль, для вывода использовать foreach.
+Создать список художник-количество произведений, созданных данным художником. Вывести список на консоль.*/
+
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            artWorck[] arr = new artWorck[]
+            {
+                new artWorck{ Creator="Ахудожник", Name="Мишки в лесу", Date= new DateTime(1950,12,12)},
+                new artWorck{ Creator="Гхудожник", Name="Мишки в поле", Date= new DateTime(1960,12,12)},
+                new artWorck{ Creator="Вхудожник", Name="Мишки в степи", Date= new DateTime(1970,1,12)},
+                new artWorck{ Creator="Бскульптор", Name="Мишки на коне", Date= new DateTime(1980,9,12)},
+                new artWorck{ Creator="Бхудожник", Name="Мишки в прыжке", Date= new DateTime(1990,11,12)},
+                new artWorck{ Creator="Аскульптор", Name="Мишки в горах", Date= new DateTime(1910,10,12)}
+            };
+            museum museum1 = new museum(arr);
+            museum1.Show();
+            Console.WriteLine("========================");
+            museum1.saveToFile("bears.txt");
+            museum1.readFile("bears.txt");
+            Console.WriteLine("========================");
+            museum1.Show();
+            Console.WriteLine("=====by=Creator=========");
+            museum1.Sort(1);
+            museum1.Show();
+            Console.WriteLine("=====by=Name============");
+            museum1.Sort(2);
+            museum1.Show();
+            Console.WriteLine("======by=Date===========");
+            museum1.Sort(3);
+            museum1.Show();
+            Console.WriteLine("=======Total==Counts====");
+            museum1.countWorksByCreators();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ exam/museum.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class museum
+    {
+        public List<artWorck> listOfWorcks { get; set; }
+        public museum()
+        {
+            listOfWorcks = new List<artWorck>();
+        }
+        public museum(artWorck[] arr) : this()
+        {
+            foreach (artWorck aw in arr)
+                listOfWorcks.Add(aw);
+        }
+
+        public void Add(artWorck obj)
+        {
+            listOfWorcks.Add(obj);
+        }
+        public void Show()
+        {
+            foreach (var aw in listOfWorcks)
+                Console.WriteLine(aw.Name + ":" + aw.Creator + ":" + aw.Date);
+        }
+
+        public void saveToFile(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (artWorck aw in listOfWorcks)
+                {
+                    sw.WriteLine(aw.Creator + ":" + aw.Name + ":" + aw.Date);
+                }
+            }
+            Console.WriteLine("файл записан");
+        }
+
+        public void readFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string str = sr.ReadLine();
+                while (str != null)
+                {
+                    string[] astr = str.Split(':');
+                    int yyy = Convert.ToInt32(astr[2].Substring(6, 4));
+                    int mmm = Convert.ToInt32(astr[2].Substring(3, 2));
+                    int dd = Convert.ToInt32(astr[2].Substring(0, 2));
+                    DateTime newDate = new DateTime(yyy, mmm, dd);
+                    listOfWorcks.Add(new artWorck { Creator = astr[0], Name = astr[1], Date = newDate });
+                    str = sr.ReadLine();
+                }
+            }
+        }
+        public void Sort(int mode)
+        {
+            switch (mode)
+            {
+                case 1:
+                    listOfWorcks.Sort((x, y) => x.Creator.CompareTo(y.Creator));
+                    break;
+                case 2:
+                    listOfWorcks.Sort((x, y) => x.Name.CompareTo(y.Name));
+                    break;
+                case 3:
+                    listOfWorcks.Sort((x, y) => x.Date.CompareTo(y.Date));
+                    break;
+                default:
+                    Console.WriteLine("invalid arg");
+                    break;
+            }
+
+        }
+        public void countWorksByCreators()
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            foreach (artWorck aw in listOfWorcks)
+            {
+                if (!dic.ContainsKey(aw.Creator)) dic.Add(aw.Creator, 1);
+                else dic[aw.Creator]++;
+            }
+            foreach (var x in dic)
+                Console.WriteLine(x.Key + " = " + x.Value);
+        }
+    }
+}
